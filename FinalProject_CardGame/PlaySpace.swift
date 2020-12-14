1d514bf7e72bf07cd0957d41254a5f0c0e6cab26
Ryan <ryan.atkinson21@d155.org>
2020-12-14 08:02:24 -0600

    score added

--- FinalProject_CardGame/PlaySpace.swift
+++ FinalProject_CardGame/PlaySpace.swift
@@ -14,6 +14,8 @@ class PlaySpace: UIViewController {
     @IBOutlet weak var bottomPlayerNumber: UILabel!
     @IBOutlet weak var starterCard: UIImageView!
     @IBOutlet weak var playerSymbol: UIImageView!
+    @IBOutlet weak var numCardsPlayerLabel: UILabel!
+    @IBOutlet weak var numCardsCompLabel: UILabel!
     
     
     @IBOutlet weak var plainCompCard: UIImageView!
@@ -24,11 +26,14 @@ class PlaySpace: UIViewController {
     
     
     var playingDeck: DeckOfCards!
-    
+    var playerCards = 0
+    var compCards = 0
+    var tieCards = 0
  override func viewDidLoad() {
      super.viewDidLoad()
      // Do any additional setup after loading the view.
     playingDeck = DeckOfCards()
+    
  }
 
     
@@ -45,7 +50,7 @@ class PlaySpace: UIViewController {
         playerSymbol.isHidden = false
         compSymbol.isHidden = false
         tempTuple = playingDeck.drawACard()
-        
+        playerNum = tempTuple.0
         topPlayerNumber.text = "\(tempTuple.0)"
         bottomPlayerNumber.text = "\(tempTuple.0)"
         
@@ -61,10 +66,40 @@ class PlaySpace: UIViewController {
         default:
             print("") //nothing
         }
+        tempTuple = playingDeck.drawACard()
+          compNum = tempTuple.0
+              topCompNumber.text = "\(tempTuple.0)"
+              bottomCompNumber.text = "\(tempTuple.0)"
+          
+              switch tempTuple.1 {
+              case .hearts:
+                compSymbol.image = UIImage(contentsOfFile: "cardHeart")
+              case .diamonds:
+                  compSymbol.image = UIImage(contentsOfFile: "cardDiamond")
+              case .clubs:
+                  compSymbol.image = UIImage(contentsOfFile: "cardClub")
+              case .spades:
+                  compSymbol.image = UIImage(contentsOfFile: "cardSpade")
+          }
+
+          if (playerNum == compNum) {
+              tieCards += 2
+          }
+          else if ( playerNum > compNum) {
+              playerCards += (2+tieCards)
+              tieCards = 0
+              numCardsPlayerLabel.text = "Number Of Cards Collected \(playerCards)/52"
+          }
+          else {
+              compCards += (2 + tieCards)
+              tieCards = 0
+              numCardsCompLabel.text = "Number Of Cards Collected \(compCards)/52"
+        
 
     }
     
 
    
 
+}
 }
